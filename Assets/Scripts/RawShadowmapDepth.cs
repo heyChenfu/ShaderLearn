51f19064fd7a5860141a8b8e1f174d7c95b000db
陈赋 <1576391213@qq.com>
2021-12-07 17:42:31 +0800

    使用CommondBuffer实现的模糊折射效果

---
+++ Assets/Scripts/RawShadowmapDepth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class RawShadowmapDepth : MonoBehaviour
+{
+    public Light m_Light;
+    RenderTexture m_ShadowmapCopy;
+
+    void Start()
+    {
+        CommandBuffer cb = new CommandBuffer();
+
+        // Change shadow sampling mode for m_Light's shadowmap.
+        cb.SetShadowSamplingMode(BuiltinRenderTextureType.CurrentActive, ShadowSamplingMode.RawDepth);
+
+        // The shadowmap values can now be sampled normally - copy it to a different render texture.
+        RenderTargetIdentifier rtID = new RenderTargetIdentifier(m_ShadowmapCopy);
+        cb.Blit(BuiltinRenderTextureType.CurrentActive, rtID);
+
+        // Execute after the shadowmap has been filled.
+        m_Light.AddCommandBuffer(LightEvent.AfterShadowMap, cb);
+
+        // Sampling mode is restored automatically after this command buffer completes, so shadows will render normally.
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(m_ShadowmapCopy, dest);
+    }
+}
