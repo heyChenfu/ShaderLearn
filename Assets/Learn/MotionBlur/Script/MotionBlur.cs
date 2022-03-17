2512e65a86d93d6d07e36188f0816ba890ff4111
陈赋 <1576391213@qq.com>
2022-03-17 17:33:23 +0800

    运动模糊的后处理效果学习

---
+++ Assets/Learn/MotionBlur/Script/MotionBlur.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+运动模糊的后处理实现方法
+*/
+[ExecuteInEditMode]
+public class MotionBlur : MonoBehaviour
+{
+    public Material motionBlurMaterial = null;
+    private RenderTexture accumulationTexture;
+
+    [Range(0.0f, 0.9f)]
+    public float blurAmount = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnDisable()
+    {
+        DestroyImmediate(accumulationTexture);
+    }
+
+        void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            if (motionBlurMaterial != null)
+            {
+                // Create the accumulation texture
+                if (accumulationTexture == null || accumulationTexture.width != src.width || accumulationTexture.height != src.height)
+                {
+                    DestroyImmediate(accumulationTexture);
+                    accumulationTexture = new RenderTexture(src.width, src.height, 0);
+                    accumulationTexture.hideFlags = HideFlags.HideAndDontSave;
+                    Graphics.Blit(src, accumulationTexture);
+                }
+
+                //恢复操作, 表示渲染到该纹理, 但是该纹理未被清空的情况
+                accumulationTexture.MarkRestoreExpected();
+
+                motionBlurMaterial.SetFloat("_BlurAmount", blurAmount);
+
+                Graphics.Blit(src, accumulationTexture, motionBlurMaterial);
+                Graphics.Blit(accumulationTexture, dest);
+            }
+            else
+            {
+                Graphics.Blit(src, dest);
+            }
+
+        }
+
+}
