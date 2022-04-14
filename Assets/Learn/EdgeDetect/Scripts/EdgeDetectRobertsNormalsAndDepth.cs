836279678d13432f980618a13ddcd80bed22be78
陈赋 <1576391213@qq.com>
2022-04-14 14:56:35 +0800

    边缘检测使用法线和深度纹理

---
+++ Assets/Learn/EdgeDetect/Scripts/EdgeDetectRobertsNormalsAndDepth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class EdgeDetectRobertsNormalsAndDepth : MonoBehaviour
+{
+    public Material _robertsOutlineMaterial;
+
+    void OnEnable() {
+		GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
+	}
+
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+    {
+        if (_robertsOutlineMaterial != null)
+        {
+            Graphics.Blit(sourceTexture, destTexture, _robertsOutlineMaterial);
+        }
+        else
+        {
+            Graphics.Blit(sourceTexture, destTexture);
+        }
+    }
+
+}
